d0759eeab27197bac33e2f0bc2c9bf2586a0b6c3
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2024-09-05 16:51:52 +0700

    Create registration controller by using generate controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,47 @@
+class RegistrationsController < ApplicationController
+  def index
+    @registrations = Registration.all
+  end
+
+  def show
+    @registration = Registration.find(params[:id])
+  end
+
+  def new
+    @registration = Registration.new
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      redirect_to @registration, notice: 'Registration was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @registration = Registration.find(params[:id])
+  end
+
+  def update
+    @registration = Registration.find(params[:id])
+    if @registration.update(registration_params)
+      redirect_to @registration, notice: 'Registration was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @registration = Registration.find(params[:id])
+    @registration.destroy
+    redirect_to registrations_url, notice: 'Registration was successfully destroyed.'
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+  end
+end
