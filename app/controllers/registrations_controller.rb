d00c103fa8e81a556e8424f77f800b6eeefc7066
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2024-09-11 13:50:24 +0700

    Add delete confirmation modal by using turbo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -33,10 +33,23 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def confirm_destroy
+    @registration = Registration.find(params[:id])
+    render turbo_stream: turbo_stream.replace(
+      "modal",
+      partial: "delete_modal",
+      locals: { registration: @registration }
+    )
+  end
+
   def destroy
     @registration = Registration.find(params[:id])
     @registration.destroy
-    redirect_to registrations_url
+
+    respond_to do |format|
+      format.turbo_stream { redirect_to registrations_url, notice: 'Registration was successfully deleted.' }
+      format.html { redirect_to registrations_url, notice: 'Registration was successfully deleted.' }
+    end
   end
 
   private
---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+  }
+
+  close() {
+    this.element.remove()
+  }
+  
+  success() {
+    this.close()
+  }
+}
